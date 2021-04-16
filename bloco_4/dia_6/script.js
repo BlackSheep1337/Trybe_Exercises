fafa3c425105cd802e8f0ebe585b54735e8aab97
Alexandre <naitwa.alexandre@gmail.com>
2021-04-16 10:06:34 -0300

    finished

--- bloco_4/dia_6/script.js
+++ bloco_4/dia_6/script.js
@@ -87,14 +87,14 @@
 // console.log(arr);
 
 
-const array = [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54];
+// const array = [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54];
 
-const arr = [];
+// const arr = [];
 
-for (let i = 0; i < array.length; i += 1) {
-    typeof array[i+1] === Number ? arr.push(array[i] * array[i+1]) : arr.push(array[i]*2);
-}
-console.log(arr);
+// for (let i = 0; i < array.length; i += 1) {
+//     typeof array[i+1] === Number ? arr.push(array[i] * array[i+1]) : arr.push(array[i]*2);
+// }
+// console.log(arr);
 
 
 
@@ -109,3 +109,123 @@ console.log(arr);
 //     }
 // }
 // console.log(array);
+
+// let pizzas = {
+//     sabor: "Palmito",
+//     preco: 39.90,
+//     bordaCatupiry: true
+// };
+
+// for (let key in pizzas) {
+//     console.log(pizzas[key]);
+// }
+
+
+// let pizzasDoces = ['chocolate', 'banana', 'morango'];
+
+// for (let key in pizzasDoces) {
+//     console.log(key, pizzasDoces[key]);
+// }
+
+// let cars = ['Saab', 'Volvo', 'BMW'];
+
+// for (let i in cars) {
+//     console.log(i, cars[i]);
+// }
+
+// let car =  {
+//     type: "Fiat",
+//     model: "500",
+//     color: "White"
+// };
+
+// for (let i in car) {
+//     console.log(i);
+// }
+
+// let food = ['hamburguer', 'bife', 'acaraje'];
+
+// for (let position in food) {
+//     console.log(position);
+// }
+
+// for (let value of food) {
+//     console.log(value);
+// }
+
+// let names = {
+//     person1: 'Joao',
+//     person2: 'Maria',
+//     person3: 'Jorge'
+// };
+
+// for (let i in names) {
+//     console.log('Ola ' + names[i]);
+// }
+
+// let carros = {
+//     model: 'A3 Sedan',
+//     manufacturer: 'Audi',
+//     year: 2020
+// };
+
+// for (let i in carros) {
+//     console.log(i + ': ' + carros[i])
+// }
+
+
+
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal dos quadrinhos do Pato Donald'
+// };
+
+// let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem: 'Christmas on Bear Mountain, Dell\'s Four Color Comics #178',
+//     nota: 'O ultimo MacPatinhas',
+//     recorrente: 'Sim'
+// };
+
+// // console.log('Bem-Vinda, ' + info.personagem);
+
+// info.recorrente = 'Sim';
+
+// console.log(info);
+
+// for (let i in info) {
+//     console.log(i);
+// }
+
+// for (let i in info, info2) {
+//     console.log(info[i] + ' e ' + info2[i]);
+// }
+
+
+
+// const palimdrome = (str) => {
+//     for (let i in str) {
+//         if (str[0].toLowerCase() === str[str.length - 1 - i]) return true;
+//         else return false;
+//     }
+// }
+// console.log(palimdrome('A mae te ama'));
+
+
+// const greatestValue = (arr) =>  arr.indexOf(Math.max.apply(null, arr));
+// console.log(greatestValue([2, 4, 6, 7, 10, 0, -3]))
+
+
+// const minValue = (arr) => arr.indexOf(Math.min.apply(null, arr));
+// console.log(minValue([2, 4, 6, 7, 10, 0, -3]))
+
+// const arrNames = (arr) => arr.reduce((a, b) => a.length > b.length ? a : b);
+// console.log(arrNames(['Jose', 'Lucas', 'Nadia', 'Fernanda', 'Cairo', 'Joana']));
+
+
+function occurrenceArr(arr) {
+    return arr;
+}
+
+console.log(occurrenceArr[2, 3, 2, 5, 8, 2, 3]);
--- bloco_6/dia_1/pratice03/script.js
+++ bloco_6/dia_1/pratice03/script.js
@@ -1,3 +1,7 @@
+const ol = document.querySelector('.items-form');
+const name = document.getElementById('input-name');
+const inputs = document.querySelectorAll('input');
+
 function addUf() {
   const element = document.getElementById('input-estado');
   const array = ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO"];
@@ -11,7 +15,12 @@ addUf();
 
 function submitForm(e) {
   //se algum dos quesitos nao bater, execute essa funcao!
-  e.preventDefault();  
+  e.preventDefault();
+  for (let i = 0; i < inputs.length; i += 1) {
+    const li = document.createElement('li');
+    li.innerHTML = inputs[i].value;
+    ol.appendChild(li);
+  }
 }
 
 function clearAllCamps(e) {
