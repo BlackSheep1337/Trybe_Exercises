12bf7b97a8ec7dda8a98115129dc83cea3b1ae83
Alexandre <naitwa.alexandre@gmail.com>
2021-05-06 09:44:14 -0300

    exercise02

--- bloco_8/dia_4/exercises/exercise01/script.js
+++ bloco_8/dia_4/exercises/exercise01/script.js
@@ -8,11 +8,11 @@ const arrays = [
 ];
 
 function flatten() {
-    const single = arrays.reduce((acc, arr) => acc.concat(arr));
-    return single;
+  const flat = arrays.reduce((acc, arr) => acc.concat(arr), []);
+  return flat;
 }
 
-
+console.log(flatten());
 //Dada uma matriz de matrizes, transforme em uma única matriz.
 
 assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
