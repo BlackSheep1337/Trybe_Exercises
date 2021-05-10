53b7ba5a2eefec996eee88790c33effb25d431ad
Alexandre <naitwa.alexandre@gmail.com>
2021-05-10 13:05:41 -0300

    pratices

--- bloco_8/dia_6/pratices/pratice02/script.js
+++ bloco_8/dia_6/pratices/pratice02/script.js
@@ -1,19 +1,56 @@
 /* Soma de numeros */
+// const numeros = [5, 12, 30, 10, 99, 4];
+
+// //for exemple;
+// let count = 0;
+// for (let i = 0; i < numeros.length; i += 1) {
+//   count += numeros[i];
+// }
+// console.log(count);
+// //reduce exemple;
+// const sum = numeros.reduce((acc, numero) => (acc + numero),0)
+// console.log(sum);
+
+// function factorial(number) {
+//   const result = Array.from({length: number}, ($, i) => i + 1)
+//                   .reduce((acc, cur) => (acc * cur), 1);
+//   return result;
+// }
+// console.log(factorial(5));
+
+
+/* Calcular Media */
+// const numeros = [5, 12, 30, 10, 99, 4];
+// let count = 0;
+// for (let i = 0; i < numeros.length; i += 1) {
+//   count += numeros[i] / numeros.length;
+// }
+// console.log(count);
+
+// const media = numeros.reduce((acc, number, i, arr) => (acc + number / arr.length),0);
+// console.log(media);
+
+/*Fazendo o mesmo que Map e Filter */
 const numeros = [5, 12, 30, 10, 99, 4];
+// const double = numeros.map((num) => num * 2);
+// const doubleReduce = numeros.reduce((acc, num) => ([...acc, num * 2]),[]);
+// console.log(doubleReduce);
+// console.log(double);
 
-//for exemple;
-let count = 0;
-for (let i = 0; i < numeros.length; i += 1) {
-  count += numeros[i];
-}
-console.log(count);
-//reduce exemple;
-const sum = numeros.reduce((acc, numero) => (acc + numero),0)
-console.log(sum);
+// const evenNumbersFilter = numeros.filter((num) => num % 2 === 0);
+// console.log(evenNumbersFilter);
 
-function factorial(number) {
-  const result = Array.from({length: number}, ($, i) => i + 1)
-                  .reduce((acc, cur) => (acc * cur), 1);
-  return result;
-}
-console.log(factorial(5));
+// const evenNumbersReduce = numeros.reduce((acc, num) => {
+//   (num % 2 === 0) ? acc.push(num) : acc;
+//   return acc.sort((a, b) => a > b);
+// }, [])
+// console.log(evenNumbersReduce);
+
+/*Flatten */
+// const numerosDeNumeros = [ [12, 40], [10, 5, 3], [99, 49, 100], [10, 20, [50, 30, 20]]];
+
+// const flatten = numerosDeNumeros.reduce((acc, numbers) => {
+//   return [...acc, ...numbers];
+// }, [])  
+// console.log(flatten);
+
---
+++ bloco_8/dia_6/pratices/pratice03/script.js
@@ -0,0 +1,103 @@
+const usuarios = [
+  {
+    id: '001',
+    nome: 'Ayrton',
+    idade: 28,
+    email: 'programadorabordo@gmail.com',
+    foto: 'https://via.placeholder.com/300/200?id=1',
+    sexo: 'M',
+    estado: 'RJ',
+  },
+  {
+    id: '002',
+    nome: 'João',
+    idade: 28,
+    email: 'joao@gmail.com',
+    foto: 'https://via.placeholder.com/300/200?id=2',
+    sexo: 'M',
+    estado: 'PE',
+  },
+  {
+    id: '003',
+    nome: 'Maria',
+    idade: 28,
+    email: 'maria@gmail.com',
+    foto: 'https://via.placeholder.com/300/200?id=3',
+    sexo: 'F',
+    estado: 'RJ',
+  },
+  {
+    id: '004',
+    nome: 'Eduardo',
+    idade: 20,
+    email: 'eduardo@gmail.com',
+    foto: 'https://via.placeholder.com/300/200?id=4',
+    sexo: 'M',
+    estado: 'RS',
+  },
+  {
+    id: '005',
+    nome: 'Antônia',
+    idade: 30,
+    email: 'antonia@gmail.com',
+    foto: 'https://via.placeholder.com/300/200?id=5',
+    sexo: 'F',
+    estado: 'RJ',
+  },
+  {
+    id: '006',
+    nome: 'Joaquina',
+    idade: 35,
+    email: 'joaquina@gmail.com',
+    foto: 'https://via.placeholder.com/300/200?id=6',
+    sexo: 'F',
+    estado: 'TO',
+  },
+  {
+    id: '007',
+    nome: 'Mateus',
+    idade: 42,
+    email: 'mateus@gmail.com',
+    foto: 'https://via.placeholder.com/300/200?id=7',
+    sexo: 'M',
+    estado: 'MG',
+  },
+  {
+    id: '008',
+    nome: 'Davi',
+    idade: 19,
+    email: 'davi@gmail.com',
+    foto: 'https://via.placeholder.com/300/200?id=8',
+    sexo: 'M',
+    estado: 'MS',
+  },
+  {
+    id: '009',
+    nome: 'Angélica',
+    idade: 27,
+    email: 'angelica@gmail.com',
+    foto: 'https://via.placeholder.com/300/200?id=9',
+    sexo: 'F',
+    estado: 'RS',
+  },
+  {
+    id: '010',
+    nome: 'Fernando',
+    idade: 31,
+    email: 'fernando@gmail.com',
+    foto: 'https://via.placeholder.com/300/200?id=10',
+    sexo: 'M',
+    estado: 'SP',
+  }
+];
+
+/*Normalizacao de dados */
+const getById = usuarios.reduce((acc, user) => {
+  acc[user.id] = user;
+  return acc;
+},{})
+
+// console.log(getById['001']);
+// console.log(getById['002']);
+
+/* Agrupamento */
