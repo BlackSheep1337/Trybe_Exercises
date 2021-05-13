95290ca29bba3382ad7755426342c5e63adcbd7f
Alexandre <naitwa.alexandre@gmail.com>
2021-05-13 12:11:19 -0300

    refactore callback hell

--- bloco_9/dia_2/pratices02/pratice01/script.js
+++ bloco_9/dia_2/pratices02/pratice01/script.js
@@ -1,20 +1,43 @@
 const fetchPokemon = () => {
-  fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+  fetch('https://pokeapi.co/api/v2/pokemon/squirtle')
     .then((response) => {
       response.json()
         .then((data) => {
           imageHandle(data);
           abilitiesHandle(data);
+          
+          fetch('https://pokeapi.co/api/v2/pokemon/bulbasaur')
+          .then((response) => {
+            response.json()
+              .then((data) => {
+                imageHandle(data);
+                abilitiesHandle(data)
+                
+                fetch('https://pokeapi.co/api/v2/pokemon/charmander')
+                .then((response) => {
+                  response.json()
+                    .then((data) => {
+                      imageHandle(data);
+                      abilitiesHandle(data)
+                      
+                    });
+                });
+              });
+          });
         });
     });
 }
 fetchPokemon(); 
 
 const imageHandle = (data) => {
+  const ul = document.querySelector('ul');
+  const li = document.createElement('li');
   const img = document.createElement('img');
-  const elem = document.getElementById('pokemon');
-  img.src = data.sprites.front_default;
-  elem.innerHTML = `${data.name} <br> ${img.outerHTML}`;
+
+  img.src = data.sprites.other.dream_world.front_default;
+  li.innerHTML = `${data.name} <br> ${img.outerHTML}`;
+
+  ul.appendChild(li);
 }
 
 const abilitiesHandle = (data) => {
