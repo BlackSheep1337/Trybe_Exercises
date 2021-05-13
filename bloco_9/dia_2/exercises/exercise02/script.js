b4eb59997d0fcf07610899a25f39733caa3f0612
Alexandre <naitwa.alexandre@gmail.com>
2021-05-13 10:08:05 -0300

    changes elementCreat

--- bloco_9/dia_2/exercises/exercise02/script.js
+++ bloco_9/dia_2/exercises/exercise02/script.js
@@ -1,22 +1,22 @@
-const plusEightThunsand = (term) => {
-  return new Promise((resolve, rejected) => {
-    const myObj = { method: 'GET', headers: { 'Accept': 'application/json' }}
-    const API_URL = `https://icanhazdadjoke.com/`;
-    const li = document.createElement('li');
-    const ul = document.getElementById('lista');
 
-    fetch(API_URL, myObj)
-      .then((r) => {
-        r.json()
-          .then((j) => {
-            li.innerHTML = j.joke;
-            ul.appendChild(li);
-            resolve()
-          })
-          .catch((error) => rejected(error));
-      })
-      .catch((error) => rejected(error));
-  })
+
+async function show8k() {
+  const myObj = {method: 'GET',headers: { 'Accept': 'application/json' }};
+  const API_URL = `https://icanhazdadjoke.com/`;
+  try {
+    const response = await fetch(API_URL, myObj);
+    const json = await response.json();
+    elementCreatHandler(json);
+  } catch (error) {
+    console.log(error);
+  }
 }
+show8k();
 
-plusEightThunsand();
+
+const elementCreatHandler = (json) => {
+  const li = document.createElement('li');
+  const ul = document.getElementById('lista');
+  li.innerHTML = json.joke;
+  ul.appendChild(li);
+}
